7d565b87e07a3c2fefb2403cc4dc2995c0b9c82e
Eduard Castro <eduard.cas07@gmail.com>
2020-10-18 15:02:11 -0500

    Building a generator of payments to process

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Currency;
+use App\PaymentGateway;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $currencies = Currency::all();
+        $paymentGateways = PaymentGateway::all();
+
+        return view('home')->with([
+            'currencies' => $currencies,
+            'paymentGateways' => $paymentGateways,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,74 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Make a payment') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        <form action="#" method="POST" id="paymentForm">
+                            @csrf
 
-                    {{ __('You are logged in!') }}
+                            <div class="row">
+                                <div class="col-auto">
+                                    <label for="value">How much you want to pay?</label>
+                                    <input type="number" id="value" min="5" step="0.01" name="value"
+                                           class="form-control"
+                                           value="{{ mt_rand(500, 100000) /100  }}"/>
+                                    <small class="form-text text-muted">
+                                        Use values with up to two decimal positions, using dot "."
+                                    </small>
+                                </div>
+
+                                <div class="col-auto">
+                                    <label for="currency">Currency</label>
+                                    <select name="currency" id="currency" class="custom-select">
+                                        @foreach($currencies as $currency)
+                                            <option
+                                                value="{{ $currency->iso }}">{{ strtoupper($currency->iso) }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="row mt-3">
+                                <div class="col">
+                                    <label>Select the desired payment platform:</label>
+                                    <div class="form-group" id="paymentGatewayToggle">
+                                        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                            @foreach($paymentGateways as $paymentGateway)
+                                                <label class="btn btn-outline-secondary rounded m-2 p-1"
+                                                       data-target="#{{ strtolower($paymentGateway->name) }}Collapse"
+                                                       data-toggle="collapse">
+                                                    <input type="radio" name="paymentGateway"
+                                                           value="{{ $paymentGateway->id }}"
+                                                           required/>
+                                                    <img src="{{ asset($paymentGateway->image) }}"
+                                                         alt="{{ $paymentGateway->name }}"
+                                                         class="img-thumbnail">
+                                                </label>
+                                            @endforeach
+                                        </div>
+                                        @foreach($paymentGateways as $paymentGateway)
+                                            <div id="{{ strtolower($paymentGateway->name) }}Collapse" class="collapse"
+                                                 data-parent="#paymentGatewayToggle">
+                                                @includeIf('components.' . strtolower($paymentGateway->name) . '-collapse')
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="text-center mt-3">
+                                <button class="btn btn-primary btn-lg" id="payButton">Pay</button>
+                            </div>
+
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
